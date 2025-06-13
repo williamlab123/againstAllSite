346da7152a9774ae0cae8db05e56c0063c49cb34
williamlab123 <williamprv@outlook.com>
2025-06-13 11:17:11 -0300

    feat - patch notes

---
+++ posts.js
@@ -0,0 +1,29 @@
+
+const patchNotesData = [
+    {
+        version: "v0.2Demo",
+        date: "June 13, 2025",
+        title: "Some enemies weaker",
+        description: `
+            <p>Enemies from level 7, 8 and 9 are now weaker in life and damage (not all of them)</p>
+            <ul>
+                <li><strong>New Shop Skin Soon:</strong> A brand new skin to make your in game shop looks great.</li>
+                <li><strong>Balance:</strong> Big enemies from level 8 had their damage reduced in 10.</li>
+                <li><strong>Bug Fix:</strong> Fixed Playfab login fail. Now you game should always login correctly.</li>
+            </ul>
+        `,
+        images: ["imgs/pixil-frame-1(1).png", "imgs/pixil-frame-0.png"] 
+
+
+    },
+    // {
+    //     version: "v1.0.0",
+    //     date: "June 29, 2025",
+    //     title: "Game Launch!",
+    //     description: `
+    //         <p>Welcome to the official launch of AgainstALL! We are so excited to have you join us on this journey. Climb the leaderboards, unlock cool skins, and survive against all odds.</p>
+    //         <p>Thank you for playing!</p>
+    //     `,
+    //     image: "" // No image for this post
+    // }
+];
